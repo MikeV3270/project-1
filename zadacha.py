85673eb2d5eb94a2b9e2ebd6047410974df42ce6
Victor Korneplod <101420065+MikeV3270@users.noreply.github.com>
2022-10-09 19:23:06 +0300

    Update zadacha.py

--- zadacha.py
+++ zadacha.py
@@ -1,13 +1,17 @@
-a = float(input('a = '))
-b = float(input('b = '))
-c = float(input('c = '))
-d = b**2 - 4 * a * c
-if d > 0:
-    x1 = (-b + (d**0.5) / (2 * a))
-    x2 = (-b - (d**0.5) / (2 * a))
-    print(x1,x2)
-elif d == 0:
-    x = -b / (2 * a)
-    print(x)
-if d < 0:
-    print("no roots")
+a = int(input('a = '))
+b = int(input('b = '))
+c = int(input('c = '))
+if a == 0:
+    x = -c/b
+    print (x) 
+if a!= 0:   
+    d = b**2 - 4 * a * c
+    if d > 0:
+        x1 = (-b + (d**0.5) / (2 * a))
+        x2 = (-b - (d**0.5) / (2 * a))
+        print(x1,x2)
+    elif d == 0:
+        x = -b / (2 * a)
+        print(x)
+    if d < 0:
+        print("no roots")
